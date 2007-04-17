31a149562710cd2bed972ebfdd7e594c7eb043c7
davi <davi>
2007-04-17 19:54:10 +0000

    Oops!  :)  Fixing the copyright.

--- Layer-5__DB_operation/Alerts.php
+++ Layer-5__DB_operation/Alerts.php
@@ -1,7 +1,7 @@
 <?php
 // Authors: Davi Leal
 //
-// Copyright (C) 2002, 2003, 2004, 2005, 2006, 2007 Davi Leal <davi at leals dot com>
+// Copyright (C) 2007 Davi Leal <davi at leals dot com>
 //
 // This program is free software; you can redistribute it and/or modify it under
 // the terms of the Affero General Public License as published by Affero Inc.,
