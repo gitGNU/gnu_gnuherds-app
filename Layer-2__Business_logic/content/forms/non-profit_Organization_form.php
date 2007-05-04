cf4c338f2303b1940dbb6b6694cd9f11154345b3
davi <davi>
2007-05-04 22:15:27 +0000

    Trying to avoid source code duplication.

--- Layer-2__Business_logic/content/forms/non-profit_Organization_form.php
+++ Layer-2__Business_logic/content/forms/non-profit_Organization_form.php
@@ -17,26 +17,14 @@
 // Suite 225, San Francisco, CA 94107, USA
 
 
-require_once "../Layer-4__DBManager_etc/DB_Manager.php";
+require_once "../Layer-2__Business_logic/content/forms/Entity_form.php";
 require_once "../Layer-4__DBManager_etc/PHP_Tools.php";
-require_once "../Layer-2__Business_logic/others/Log_form.php";
 
 // Methods take the values from the global $_POST[] array.
 
 
-class NonprofitOrganizationForm
+class NonprofitOrganizationForm extends EntityForm
 {
-	private $manager;
-	private $processingResult;
-
-
-	function __construct()
-	{
-		$this->manager = new DBManager();
-		$this->processingResult = '';
-	}
-
-
 	public function processForm()
 	{
 		$phpTools = new PHPTools();
@@ -254,124 +242,5 @@ class NonprofitOrganizationForm
 
 		return true;
 	}
-
-
-
-	private function register()
-	{
-		if ( $this->manager->lookForEntity($_GET['email']) == true )
-		{
-			// Check to avoid spam
-			if ( $this->manager->allowActivateAccountEmail($_GET['email']) == true )
-			{
-				// Send a warning email
-				$message = gettext("An attempt was made to activate a new GNU Herds account with this email address.")." ".gettext("However, you have already an active account! Follow the below link to get your lost password if it is needed:")."\n\n";
-
-				$message .= "https://".$_SERVER['HTTP_HOST']."/Lost_Password.php";
-
-				$message .= "\n\n";
-				$message .= gettext("If you have not asked for this new account, someone else has asked for it with your email!")."\n\n";
-
-				mail($_GET['email'], "GNU Herds: ".gettext("Lost password?"), "$message", "From: association@gnuherds.org");
-			}
-
-			// Raise the usual error message
-			$error = "<p>".gettext("ERROR: Wrong magic number!.")."</p>";
-			throw new Exception($error,false);
-		}
-		else
-		{
-			// Try to activate a pending account
-			$new_password = $this->manager->activateAccountForEntity();
-
-			if ( $new_password != false )
-			{
-				$this->processingResult .= "<p>&nbsp;</p>\n";
-				$this->processingResult .= "<p>&nbsp; &nbsp; &nbsp; &nbsp; ".gettext("Your account has been activated!")."</p>\n";
-
-				// Show the password
-				$this->processingResult .= "<p>&nbsp;</p>\n";
-				$this->processingResult .= "<p>&nbsp; &nbsp; &nbsp; &nbsp; ".gettext("Your new password is:")." <strong>".$new_password."</strong></p>\n";
-				$this->processingResult .= "<p>&nbsp; &nbsp; &nbsp; &nbsp; ".gettext("To improve your security, you should change your password after loging in.")."</p>";
-			}
-		}
-	}
-
-	private function changeEmail()
-	{
-		// Try to change the account's email
-		// The entity has to be logged, else it will fail at the Layer-5.
-		if ( $this->manager->changeEmailForEntity() == true )
-		{
-			$this->processingResult .= "<p>&nbsp;</p>\n";
-			$this->processingResult .= "<p>&nbsp; &nbsp; &nbsp; &nbsp; ".gettext("The email has been changed!")."</p>\n";
-		}
-	}
-
-	private function requestChangeEmail()
-	{
-		// Make the 'magic' flag
-		$magic = md5( rand().rand().rand().rand().rand().rand().rand().rand().rand().rand().rand() );
-
-		// Keep the 'magic' in the data base
-		$this->manager->saveWantEmailMagicForEntity($magic);
-
-		// Spam is not possible due to this operation is only raised by the entity when he/she is logged.
-		// Send the email
-		$message .= gettext("To change your GNU Herds account's email, first log in and then follow the below link.")." ".gettext("That link will expire in 7 days:")."\n\n";
-
-		$message .= "https://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']."?email=".$_POST['Email']."&magic=".$magic;
-
-		$message .= "\n\n";
-		$message .= gettext("If you have not asked for it, just ignore this email.")."\n\n";
-
-		mail($_POST['Email'], "GNU Herds: ".gettext("Change account's email"), "$message", "From: association@gnuherds.org");
-
-		// Report to the user
-		$this->processingResult .= "<p>&nbsp;</p><p>".gettext('An email has been sent to the new email address to validate it.')."<p>\n";
-	}
-
-	private function requestRegister()
-	{
-		if ( $this->manager->lookForEntity($_POST['Email']) == true )
-		{
-			// Check to avoid spam
-			if ( $this->manager->allowRegisterAccountDuplicatedEmail($_POST['Email']) == true )
-			{
-				$message = gettext("An attempt was made to register a new GNU Herds account with this email address.")." ".gettext("However, you have already an active account! Follow the below link to get your lost password if it is needed:")."\n\n";
-
-				$message .= "https://".$_SERVER['HTTP_HOST']."/Lost_Password.php";
-
-				$message .= "\n\n";
-				$message .= gettext("If you have not asked for this new account, someone else has asked for it with your email!")."\n\n";
-
-				mail($_POST['Email'], "GNU Herds: ".gettext("Lost password?"), "$message", "From: association@gnuherds.org");
-			}
-		}
-		else
-		{
-			// Make the 'magic' flag
-			$magic = md5( rand().rand().rand().rand().rand().rand().rand().rand().rand().rand().rand() );
-
-			$this->manager->addEntity($magic);
-
-			// Send the email
-			$message = gettext("Your email has been used to create an account at GNU Herds.")."\n\n";
-
-			$message .= gettext("To activate it follow the below link.")." ".gettext("That link will expire in 24 hours:")."\n\n";
-
-			$message .= "https://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']."?action=register&email=".$_POST['Email']."&magic=".$magic;
-
-			$message .= "\n\n";
-			$message .= gettext("If you have not asked for this new account, ignore this email.")."\n\n";
-
-			$message .= gettext("Note: To avoid 'Spam' you can only get this email at the most once each 48 hours. If this email is Spam for you, please let it knows to  association AT gnuherds.org")."\n\n";
-
-			mail($_POST['Email'], "GNU Herds: ".gettext("Activate account"), "$message", "From: association@gnuherds.org");
-		}
-
-		// Report to the user
-		$this->processingResult .= "<p>&nbsp;</p><p>".gettext('Success. An email has been sent to such email address with the instructions to activate the account.')."<p>\n";
-	}
 }
-?> 
+?>
