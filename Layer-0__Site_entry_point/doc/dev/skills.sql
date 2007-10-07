3c6ad4d0d1e16d1c7137fa1ebb47a8a5424950fd
davi <davi>
2007-10-07 12:59:11 +0000

    The data base have to be loaded with some Skills to avoid webapp crashes. This file is used to load it in development environments.
    Ref.: wiki page: "The PostgreSQL data base"
      http://savannah.nongnu.org/cookbook/?func=detailitem&item_id=154

---
+++ Layer-0__Site_entry_point/doc/dev/skills.sql
@@ -0,0 +1,76 @@
+INSERT INTO LI_Skills VALUES ( 'FreeBSD',				'Free',		'Software distributions' );
+INSERT INTO LI_Skills VALUES ( 'NetBSD',				'Free',		'Software distributions' );
+INSERT INTO LI_Skills VALUES ( 'OpenBSD',				'Free',		'Software distributions' );
+INSERT INTO LI_Skills VALUES ( 'GNU/Hurd',				'Free',		'Operating systems' );
+INSERT INTO LI_Skills VALUES ( 'GNU/Linux',				'Free',		'Operating systems' );
+INSERT INTO LI_Skills VALUES ( 'Darwin',				'Free',		'Operating systems' );
+INSERT INTO LI_Skills VALUES ( 'Microsoft Windows',			'Non-Free',	'Operating systems' );
+INSERT INTO LI_Skills VALUES ( 'Microsoft Windows Vista',		'Non-Free',	'Operating systems' );
+INSERT INTO LI_Skills VALUES ( 'Mac OS X',				'Non-Free',	'Operating systems' );
+
+INSERT INTO LI_Skills VALUES ( 'FreeBSD kernel',			'Free',		'Operating system kernels' );
+INSERT INTO LI_Skills VALUES ( 'NetBSD kernel',				'Free',		'Operating system kernels' );
+INSERT INTO LI_Skills VALUES ( 'OpenBSD kernel',			'Free',		'Operating system kernels' );
+INSERT INTO LI_Skills VALUES ( 'GNU Hurd kernel',			'Free',		'Operating system kernels' );
+INSERT INTO LI_Skills VALUES ( 'Linux kernel',				'Free',		'Operating system kernels' );
+
+INSERT INTO LI_Skills VALUES ( 'Intel x86 / IA-32 (i386)',		'Free',		'Hardware architectures' );
+INSERT INTO LI_Skills VALUES ( 'Sun SPARC (sparc)',			'Free',		'Hardware architectures' );
+
+INSERT INTO LI_Skills VALUES ( 'C',					'Free',		'Development formal languages' );
+INSERT INTO LI_Skills VALUES ( 'C#',					'Free',		'Development formal languages' );
+INSERT INTO LI_Skills VALUES ( 'C++',					'Free',		'Development formal languages' );
+INSERT INTO LI_Skills VALUES ( 'Java',					'Free',		'Development formal languages' );
+INSERT INTO LI_Skills VALUES ( 'JavaScript',				'Free',		'Development formal languages' );
+INSERT INTO LI_Skills VALUES ( 'Lisp',					'Free',		'Development formal languages' );
+INSERT INTO LI_Skills VALUES ( 'Pascal',				'Free',		'Development formal languages' );
+INSERT INTO LI_Skills VALUES ( 'Perl',					'Free',		'Development formal languages' );
+INSERT INTO LI_Skills VALUES ( 'PHP',					'Free',		'Development formal languages' );
+INSERT INTO LI_Skills VALUES ( 'Prolog',				'Free',		'Development formal languages' );
+INSERT INTO LI_Skills VALUES ( 'Python',				'Free',		'Development formal languages' );
+INSERT INTO LI_Skills VALUES ( 'Ruby',					'Free',		'Development formal languages' );
+INSERT INTO LI_Skills VALUES ( 'Scheme',				'Free',		'Development formal languages' );
+INSERT INTO LI_Skills VALUES ( 'Shell script',				'Free',		'Development formal languages' );
+INSERT INTO LI_Skills VALUES ( 'Structured Query Language (SQL)',	'Free',		'Development formal languages' );
+
+INSERT INTO LI_Skills VALUES ( 'Unified Modeling Language (UML)',	'Free',		'Development specification languages' );
+INSERT INTO LI_Skills VALUES ( 'User-Mode Linux (UML)',			'Free',		'' );
+
+INSERT INTO LI_Skills VALUES ( 'GNU Autoconf',				'Free',		'Development tools' );
+INSERT INTO LI_Skills VALUES ( 'GNU Automake',				'Free',		'Development tools' );
+
+INSERT INTO LI_Skills VALUES ( 'Apache Ant',				'Free',		'Development tools' );
+INSERT INTO LI_Skills VALUES ( 'Apache Maven',				'Free',		'Development tools' );
+INSERT INTO LI_Skills VALUES ( 'Apache Gump',				'Free',		'Development tools' );
+
+INSERT INTO LI_Skills VALUES ( 'Lex',					'Free',		'Development tools' );
+INSERT INTO LI_Skills VALUES ( 'Yacc',					'Free',		'Development tools' );
+INSERT INTO LI_Skills VALUES ( 'Flex',					'Free',		'Development tools' );
+INSERT INTO LI_Skills VALUES ( 'Bison',					'Free',		'Development tools' );
+
+INSERT INTO LI_Skills VALUES ( 'ht://Dig',				'Free',		'Development tools' );
+INSERT INTO LI_Skills VALUES ( 'SWISH++',				'Free',		'Development tools' );
+INSERT INTO LI_Skills VALUES ( 'SWISH-E',				'Free',		'Development tools' );
+
+INSERT INTO LI_Skills VALUES ( 'GNU Emacs',				'Free',		'Development tools' );
+INSERT INTO LI_Skills VALUES ( 'vi',					'Free',		'Development tools' );
+
+INSERT INTO LI_Skills VALUES ( 'i18n tools',				'Free',		'Development tools' );
+
+INSERT INTO LI_Skills VALUES ( 'KDE',					'Free',		'Development libraries' );
+INSERT INTO LI_Skills VALUES ( 'GNOME',					'Free',		'Development libraries' );
+INSERT INTO LI_Skills VALUES ( 'GTK+',					'Free',		'Development libraries' );
+INSERT INTO LI_Skills VALUES ( 'Motif/LessTiff',			'Free',		'Development libraries' );
+
+INSERT INTO LI_Skills VALUES ( 'Common Gateway Interface (CGI)',	'Free',		'Specifications' );
+INSERT INTO LI_Skills VALUES ( 'Hypertext Markup Language (HTML)',	'Free',		'Specifications' );
+INSERT INTO LI_Skills VALUES ( 'Extensible Markup Language (XML)',	'Free',		'Specifications' );
+INSERT INTO LI_Skills VALUES ( 'Cascading Style Sheets (CSS)',		'Free',		'Specifications' );
+
+INSERT INTO LI_Skills VALUES ( 'Apache Geronimo',			'Free',		'Network servers' );
+INSERT INTO LI_Skills VALUES ( 'Apache HTTP',				'Free',		'Network servers' );
+INSERT INTO LI_Skills VALUES ( 'Apache Tomcat',				'Free',		'Network servers' );
+
+INSERT INTO LI_Skills VALUES ( 'Doxygen',				'Free',		'Documentation tools' );
+INSERT INTO LI_Skills VALUES ( 'LaTeX',					'Free',		'Documentation tools' );
+INSERT INTO LI_Skills VALUES ( 'Texinfo',				'Free',		'Documentation tools' );
