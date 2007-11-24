fe5dac1e426d89e61b39d89ec37cd68e161b7ff1
Davi Leal <davi@leals.com>
2007-11-25 00:17:44 +0100

    Updating the license from AGPLv1 to AGPL2, and from AGPL2 to GNU AGPLv3
    The licenses allow us to apply the below license upgrade path even without being the copyright holder, due to the  "or (at your option) any later version" text is present at each licensed file:
    
      AGPLv1   -->   AGPL2   -->   GNU AGPLv3
    
    * AGPLv1,     http://www.affero.org/oagpl.html
    * AGPL2,      http://www.affero.org/agpl2.html
    * GNU AGPLv3, http://www.fsf.org/licensing/licenses/agpl-3.0.html
    
    Any change that is not allowed by the "license upgrade path" must be
    explicitly agreed by _all_ copyright holders.
    
    We move directly from AGPLv1 to GNU AGPLv3 and just expose here that we have used AGPL2 as the upgrade bridge.
      Reference to the development email list: http://lists.gnu.org/archive/html/gnuherds-app-dev/2007-11/msg00056.html
    
    AGPL2,      http://www.affero.org/agpl2.html
    =====
    
    Note: The GNU Affero General Public License version 3 has been released. It follows the same basic structure as version 1 of the Affero GPL, but it is based on version 3 of the GNU GPL, and you can combine modules released under GPLv3 and the GNU AGPLv3. If you were thinking about releasing your software under this license, we recommend you use the GNU AGPLv3 instead.
    
    AFFERO GENERAL PUBLIC LICENSE
    Version 2, November 2007
    Copyright © 2007 Affero Inc.
    510 Third Street - Suite 225, San Francisco, CA 94107, USA
    
    This is version 2 of the Affero General Public License. It gives each licensee permission to distribute the Program or a work based on the Program (as defined in version 1 of the Affero GPL) under the GNU Affero General Public License, version 3 or any later version.
    
    If the Program was licensed under version 1 of the Affero GPL "or any later version", no additional obl

--- Layer-2__Business_logic/content/static/Home_content.php
+++ Layer-2__Business_logic/content/static/Home_content.php
@@ -3,18 +3,17 @@
 //
 // Copyright (C) 2006, 2007 Davi Leal <davi at leals dot com>
 //
-// This program is free software; you can redistribute it and/or modify it under
-// the terms of the Affero General Public License as published by Affero Inc.,
-// either version 1 of the License, or (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful to the Free
-// Software community, but WITHOUT ANY WARRANTY; without even the implied
-// warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the Affero
+// This program is free software: you can redistribute it and/or modify it under
+// the terms of the GNU Affero General Public License as published by the Free Software Foundation,
+// either version 3 of the License, or (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied
+// warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero
 // General Public License for more details.
-//
-// You should have received a copy of the Affero General Public License with this
-// software in the ./AfferoGPL file; if not, write to Affero Inc., 510 Third Street,
-// Suite 225, San Francisco, CA 94107, USA
+// 
+// You should have received a copy of the GNU Affero General Public License along with this
+// program in the COPYING file.  If not, see <http://www.gnu.org/licenses/>.
 
 
 class HomeContent
