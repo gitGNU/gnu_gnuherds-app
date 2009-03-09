f6229565ea4ef44f3bd4a03f3563b60e880bf0da
Davi Leal <davi@leals.com>
2009-03-09 14:32:30 +0100

    Send warning email about "Pending to classify Skills"
    It was task: http://savannah.nongnu.org/task/?8910

---
+++ crontabs/raise_Warnings_to_attend_Pending-to-classify-Skills.php
@@ -0,0 +1,24 @@
+<?php
+// Authors: Davi Leal
+// 
+// Copyright (C) 2009 Davi Leal <davi at leals dot com>
+// 
+// This program is free software: you can redistribute it and/or modify it under
+// the terms of the GNU Affero General Public License as published by the Free Software Foundation,
+// either version 3 of the License, or (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied
+// warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero
+// General Public License for more details.
+// 
+// You should have received a copy of the GNU Affero General Public License along with this
+// program in the COPYING file.  If not, see <http://www.gnu.org/licenses/>.
+
+
+require_once "../Layer-4__DBManager_etc/DB_Manager.php";
+
+
+$manager = new DBManager();
+$manager->raisePendingSkillsWarning();
+?> 
