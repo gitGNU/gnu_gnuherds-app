70c61ce67a8b4a5f79b0e81da98b5c5460236d48
Davi Leal <davi@leals.com>
2009-03-12 12:17:04 +0100

    Be able to show WantEmail for Donators too

--- Layer-5__DB_operation/Donation.php
+++ Layer-5__DB_operation/Donation.php
@@ -35,20 +35,21 @@ class Donation
 
 	public function getDonators($JobOfferId)
 	{
-		$sqlQuery = "PREPARE query(integer) AS  SELECT D1_Donation,E1_Email,EP_FirstName,EP_LastName,EP_MiddleName,EC_CompanyName,EO_OrganizationName FROM D1_Donations2JobOffers,E1_Entities WHERE D1_E1_Id=E1_Id AND D1_J1_Id=$1 ;  EXECUTE query('$JobOfferId');";
+		$sqlQuery = "PREPARE query(integer) AS  SELECT D1_Donation,E1_Email,E1_WantEmail,EP_FirstName,EP_LastName,EP_MiddleName,EC_CompanyName,EO_OrganizationName FROM D1_Donations2JobOffers,E1_Entities WHERE D1_E1_Id=E1_Id AND D1_J1_Id=$1 ;  EXECUTE query('$JobOfferId');";
 		$result = $this->postgresql->getPostgreSQLObject($sqlQuery,1);
 
 		$array['Donation'] = pg_fetch_all_columns($result, 0);
 
 		$array['Email'] = pg_fetch_all_columns($result, 1);
+		$array['WantEmail'] = pg_fetch_all_columns($result, 2);
 
-		$array['FirstName'] = pg_fetch_all_columns($result, 2);
-		$array['LastName'] = pg_fetch_all_columns($result, 3);
-		$array['MiddleName'] = pg_fetch_all_columns($result, 4);
+		$array['FirstName'] = pg_fetch_all_columns($result, 3);
+		$array['LastName'] = pg_fetch_all_columns($result, 4);
+		$array['MiddleName'] = pg_fetch_all_columns($result, 5);
 
-		$array['CompanyName'] = pg_fetch_all_columns($result, 5);
+		$array['CompanyName'] = pg_fetch_all_columns($result, 6);
 
-		$array['NonprofitName'] = pg_fetch_all_columns($result, 6);
+		$array['NonprofitName'] = pg_fetch_all_columns($result, 7);
 
 		return $array;
 	}
